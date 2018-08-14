8cb9a156736a386dfc72ad73fea167c5be3b5e8a
陈伟杰 <chenweijie@chenweijiedeiMac.local>
2018-08-14 17:27:19 +0800

    测试pod自定义库

---
+++ PodTest/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  PodTest
+//
+//  Created by 陈伟杰 on 2018/8/14.
+//  Copyright © 2018年 陈伟杰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+- (void)saySomething;
+@end
---
+++ PodTest/Person.m
@@ -0,0 +1,15 @@
+//
+//  Person.m
+//  PodTest
+//
+//  Created by 陈伟杰 on 2018/8/14.
+//  Copyright © 2018年 陈伟杰. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+- (void)saySomething{
+    NSLog(@"say hello world");
+}
+@end
